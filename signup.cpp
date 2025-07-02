146ed3b4b9e19c6b8e0a63a44156a46206891be0
smileynm <smileynm@naver.com>
2025-07-02 23:11:16 +0900

    add password echo in signUp

--- signup.cpp
+++ signup.cpp
@@ -6,6 +6,7 @@ SignUp::SignUp(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::SignUp)
 {
+    // 중복 ID 여부 확인 변수
     allow_flag = 0;
     ui->setupUi(this);
 }
@@ -28,15 +29,15 @@ void SignUp::on_dupButton_clicked()
 
     // 중복 발생시
     // if( 중복 )
-    ui->dupLabel->setStyleSheet("color: red;");
-    ui->dupLabel->setText("중복된 아이디 입니다");
-    allow_flag = 0;
+    //ui->dupLabel->setStyleSheet("color: red;");
+    //ui->dupLabel->setText("중복된 아이디 입니다");
+    //allow_flag = 0;
 
 
     // else // 신규가입
-    //ui->dupLabel->setStyleSheet("color: green;");
-    //ui->dupLabel->setText("사용가능");
-    //allow_flag = 1;
+    ui->dupLabel->setStyleSheet("color: green;");
+    ui->dupLabel->setText("사용가능");
+    allow_flag = 1;
 
 }
 
@@ -50,8 +51,34 @@ void SignUp::on_confirmButton_clicked()
     // PW와 chPW가 동일할 경우,
     // 입력된 ID와 PW를 신규로 member Vector에 저장
 
-    LogIn *login = new LogIn;
-    login->show();
-    this->close();
+    // if( pw != ckpw )
+    // "패스워드가 일치하지 않습니다"
+    ui->ckpwLabel->setStyleSheet("color: red;");
+    ui->ckpwLabel->setText("패스워드가 일치하지 않습니다");
+    allow_flag = 0;
+
+    //LogIn *login = new LogIn;
+    //login->show();
+    //this->close();
+}
+
+
+void SignUp::on_pwEdit_cursorPositionChanged(int arg1, int arg2)
+{
+
+    // 패스워드 안보이게 하기
+    ui->pwEdit->setEchoMode(QLineEdit::Password);
+    ui->ckpwEdit->setEchoMode(QLineEdit::Password);
+
+}
+
+
+void SignUp::on_ckpwEdit_cursorPositionChanged(int arg1, int arg2)
+{
+
+    // 패스워드 안보이게 하기
+    ui->pwEdit->setEchoMode(QLineEdit::Password);
+    ui->ckpwEdit->setEchoMode(QLineEdit::Password);
+
 }
 
--- signup.h
+++ signup.h
@@ -21,6 +21,10 @@ private slots:
     void on_dupButton_clicked();
     void on_confirmButton_clicked();
 
+    // 비밀번호가 보이지 않게 하기위한 slot
+    void on_pwEdit_cursorPositionChanged(int arg1, int arg2);
+    void on_ckpwEdit_cursorPositionChanged(int arg1, int arg2);
+
 private:
     Ui::SignUp *ui;
     bool allow_flag;        // 회원가입 가능한 아이디 플래그
